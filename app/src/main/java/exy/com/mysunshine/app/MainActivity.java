e44ceb958343d2bc7b04035c6b0fc381f481f867
eXy <proexy@gmail.com>
2015-02-19 19:06:46 +0100

    Add database SQLite store data Ajout des contentProvider Ajout des contrats pour la bdd Ajout des Cursor Ajout des Loader

--- app/src/main/java/exy/com/mysunshine/app/MainActivity.java
+++ app/src/main/java/exy/com/mysunshine/app/MainActivity.java
@@ -1,10 +1,8 @@
 package exy.com.mysunshine.app;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -14,14 +12,19 @@ import android.view.MenuItem;
 public class MainActivity extends ActionBarActivity {
 
     private final String LOG_TAG = MainActivity.class.getSimpleName();
+    
+    private final String FORECASTFRAGMENT_TAG = "FFTAG";
+    
+    private String mLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mLocation = Utility.getPreferredLocation(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment())
+                    .add(R.id.container, new ForecastFragment(), FORECASTFRAGMENT_TAG)
                     .commit();
         }
     }
@@ -55,11 +58,8 @@ public class MainActivity extends ActionBarActivity {
     }
     
     private void openPreferredLocationInMap() {
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String location = sharedPreferences.getString(
-                getString(R.string.pref_location_key),
-                getString(R.string.pref_location_default));
-
+        String location = Utility.getPreferredLocation(this);
+        
         Uri geoLocation = Uri.parse("geo:0,07").buildUpon()
                 .appendQueryParameter("q", location)
                 .build();
@@ -71,6 +71,21 @@ public class MainActivity extends ActionBarActivity {
             startActivity(intent);
         } else {
             Log.d(LOG_TAG, "Couldn't call " + location + ", no");
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String location = Utility.getPreferredLocation(this);
+        // update the location in our second pane using the fragment manager
+        if (location != null && !location.equals(mLocation)) {
+            ForecastFragment ff = (ForecastFragment) getSupportFragmentManager()
+                    .findFragmentByTag(FORECASTFRAGMENT_TAG);
+            if (ff != null) {
+                ff.onLocationChanged();
+            }
+            mLocation = location;
         }
     }
 }
